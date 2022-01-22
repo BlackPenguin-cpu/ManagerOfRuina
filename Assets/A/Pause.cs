f66b6051075b14f224e1c58e71ce1173363df9fa
AUryeong <jykee7030@gmail.coma>
2022-01-22 20:19:39 +0900

    일시정지 제작

---
+++ Assets/A/Pause.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Pause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseobj;
+    public void OnClick()
+    {
+        Time.timeScale = 0;
+        pauseobj.SetActive(true);
+    }
+    public void OnClick2()
+    {
+    }
+    public void OnClick3()
+    {
+        Time.timeScale = 1;
+        pauseobj.SetActive(false);
+    }
+    public void OnClick4()
+    {
+        Application.Quit();
+    }
+}
