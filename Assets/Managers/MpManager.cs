f379577086f8ddaeed00a3a2a50977615e0d0a1f
Black_Penguin <dkstmdwnswns@gmail.com>
2022-01-23 02:08:02 +0900

    mpManager

--- Assets/Managers/MpManager.cs
+++ Assets/Managers/MpManager.cs
@@ -8,7 +8,10 @@ using DG.Tweening;
 public class MpManager : MonoBehaviour
 {
     [SerializeField] Image SkillBar;
+    [SerializeField] Image PowerBar;
     bool SkillBarOn;
+    bool PowerBarOn;
+    [SerializeField] Sprite[] Barsprites;
 
     int ManaLevelupCost = 100;
     int ManaLevelupLevel = 1;
@@ -18,7 +21,10 @@ public class MpManager : MonoBehaviour
     int MaxManaLevel = 1;
     [SerializeField] TextMeshProUGUI MaxMpText;
 
-    int YanPowerCost = 200;
+    int YanPowerCost = 150;
+    [SerializeField] TextMeshProUGUI YanPowerText;
+
+    
 
     public void OnOffButton()
     {
@@ -26,11 +32,28 @@ public class MpManager : MonoBehaviour
         {
             SkillBar.transform.DOLocalMoveX(258, 1).SetEase(Ease.InOutBack);
             SkillBarOn = true;
+            SkillBar.sprite = Barsprites[1];
         }
         else
         {
+            SkillBar.sprite = Barsprites[0];
             SkillBar.transform.DOLocalMoveX(940, 1).SetEase(Ease.InOutBack);
             SkillBarOn = false;
+        }
+    }
+    public void OnOffPowerButton()
+    {
+        if (!PowerBarOn)
+        {
+            PowerBar.sprite = Barsprites[1];
+            PowerBar.transform.DOLocalMoveX(-258, 1).SetEase(Ease.InOutBack);
+            PowerBarOn = true;
+        }
+        else
+        {
+            PowerBar.sprite = Barsprites[0];
+            PowerBar.transform.DOLocalMoveX(-940, 1).SetEase(Ease.InOutBack);
+            PowerBarOn = false;
         }
     }
     public void RegenerateMpLevelUp()
@@ -58,7 +81,7 @@ public class MpManager : MonoBehaviour
     {
         if (GameManager.Instance.MP >= YanPowerCost)
         {
-            GameManager.Instance.MP -= 200;
+            GameManager.Instance.MP -= 150;
             StopCoroutine(YanPowerCoroutine);
             YanPowerCoroutine = StartCoroutine(YanPowerDelay());
         }
@@ -66,8 +89,10 @@ public class MpManager : MonoBehaviour
     IEnumerator YanPowerDelay()
     {
         GameManager.Instance.MaxValue = true;
+        YanPowerText.gameObject.SetActive(true);
         yield return new WaitForSeconds(15);
         GameManager.Instance.MaxValue = false;
+        YanPowerText.gameObject.SetActive(false);
     }
 
 }
